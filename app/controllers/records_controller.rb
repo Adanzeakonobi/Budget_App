23538f31510f09f7fb9f85887a649d53b9a8a54b
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-06 12:00:49 -0600

    Add Devise view

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,5 @@
 class RecordsController < ApplicationController
+    before_action :authenticate_user!
     def index
     end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
