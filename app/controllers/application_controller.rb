2daadbda8ca04c7db00270a1cabd49173be2078f
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-06 12:36:01 -0600

    Add user to the routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
