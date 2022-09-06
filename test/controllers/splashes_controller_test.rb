7879eb9fbccdb79cc7ca82356601028886ec8a1b
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-06 11:53:07 -0600

    Add Rspec to Gem

--- test/controllers/splashes_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SplashesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/categories_record_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CategoriesRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
