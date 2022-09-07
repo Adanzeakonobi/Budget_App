ecd82d344b641b7b19a857dec1cc1989db779fcf
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-07 14:52:05 -0600

    Add active storage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,29 @@ class CategoriesController < ApplicationController
   
     def show
     end
+
+    def new; end
+
+    def create
+      category_name = category_params['name'].downcase
+      category = current_user.categories.new(name: category_name, icon: category_params['icon'])
+      if Category.where(name: category_name).exists?
+        redirect_to new_category_url, notice: "This category name already exists"
+        return
+      else
+        if category.save
+          flash[:success] = 'You successfully added a category to your application'
+          redirect_to categories_url
+        else
+          flash.now[:error] = 'Error: You could not create a new category'
+          render :new
+        end
+      end
+    end
+  
+    private
+  
+    def category_params
+      params.require(:add_category).permit(:name, :icon)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
     has_many :categories_records
     has_many :records, through: :categories_records
     has_one_attached :icon
+    before_save { name.downcase! }
   
     validates :name, presence: true, length: { maximum: 100 }
     validates :icon, presence: true, length: { maximum: 250 }
