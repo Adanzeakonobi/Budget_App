ee575ed3fb14173e583aeabf29db72cbb310a4d2
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-08 15:39:36 -0600

    Modify styling for category

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,35 +1,40 @@
 class RecordsController < ApplicationController
-    load_and_authorize_resource
-    before_action :authenticate_user!
-    
-    def index
-        @category = current_user.categories.includes(:records).find(params[:category_id])
-    end
-  
-    def show
-    end
+  load_and_authorize_resource
+  before_action :authenticate_user!
 
-    def new; end
+  def index
+    @category = current_category
+  end
+
+  def new
+    @category = current_category
+  end
 
   def create
     record_name = record_params['name']
     record_amount = record_params['amount']
     category_ids = record_params['category_ids'].slice(1..-1)
-    first_category = Category.find_by(id: record_params['category_ids'][1].to_i)
+    category = current_category
     @record = current_user.records.new(name: record_name, amount: record_amount)
     if @record.save
       category_ids.each do |id|
         @record.categories_records.create!(category_id: id.to_i)
       end
-      flash[:success] = "You completed this transaction successfully"
-      redirect_to category_records_path(first_category.id)
+      flash[:success] = 'You completed this transaction successfully'
+      redirect_to category_records_path(category)
     else
-      flash.now[:error] = "New transaction could not be created."
+      flash.now[:error] = 'New transaction could not be created.'
       render :new
     end
   end
 
+  def current_category
+    @category = current_user.categories.includes(:records).find(params[:category_id])
+  end
+
+  private
+
   def record_params
-    params.require(:add_record).permit(:name, :amount, category_ids:[])
+    params.require(:add_record).permit(:name, :amount, category_ids: [])
   end
 end
