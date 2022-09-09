01152355f629cf84493e851bbe7ad81d1128cb23
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-09 17:44:23 -0600

    Modified the view of the hompage

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,6 @@ class Category < ApplicationRecord
     has_many :categories_records
     has_many :records, through: :categories_records
     has_one_attached :icon
-    before_save { name.downcase! }
   
     validates :name, presence: true, length: { maximum: 100 }
     validates :icon, presence: true, length: { maximum: 250 }
